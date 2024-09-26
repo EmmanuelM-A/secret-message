3ffc0ec3e9e270e6a267094814f70d25d2f482a5
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2024-09-26 09:33:28 +0100

    Working on the encryption

--- Encryption.py
+++ Encryption.py
@@ -2,20 +2,23 @@ from Mappings import mapping_table
 import random
 
 def encrypt_message(message):
-    key = []
-    encrypted_message = []
+    pass
 
-    for _ in message:
-        num = random.randint(0, len(mapping_table))
 
-        encrypted_message.append(mapping_table[num])
+# Iterate through the message getting each character
 
-        key.append(num)
+# Match each character to its mapping value
 
-    encrypted_message = str(encrypted_message)
+# Randomly select replacement character(s)
 
-    print(f"Encrypted message: {encrypted_message}")
+# Take record of the character subsituted and index of the replacement to form the key: [[char, sub], [char2, sub2], [char3, sub2]]
 
-    print(f"Key: {key}")
+# Once complete, combine the key elements into one string
 
-    
+# Shift the characters left in the string by N amount
+
+# Append that N amount onto the end of the key string: .............N
+
+# Print the cipher_text
+
+# Print the key - Used to decrypt the cipher_text
